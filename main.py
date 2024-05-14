79e1cd90fd0cf0a653e9214ac38cd4dad306a080
Елена Контаурова <145374221+Elena-kontaurova@users.noreply.github.com>
2024-05-14 09:03:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from bs4 import BeautifulSoup
-import requests
+''' Файл для запуска бота - анкеты'''
+import asyncio
 
-url = f'https://kartaslov.ru/книги/500_самых_свежих_анекдотов/1'
-response = requests.get(url)
-bs = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
-texts = []
+from aiogram import Bot, Dispatcher
+from handlers import include_routers
 
-for elem in bs.find("div", attrs={'class': 'book-section'}).findAll():
-    if elem["class"][0] == "book-sub-title":
-        texts.append("")
-    else:
-        texts[-1] += elem.text
+bot = Bot(token='6873870024:AAHVnZXANpYSIZOg-jcQ0Yd8ZcHF1Ebxvv0')
+dp = Dispatcher()
 
-for text in texts:
-    print(text)
-    print()
+async def main():
+    ''' Метод запускает процесс опроса, входящих обновлений для бота. 
+Основаная точка входа и запуска бота. '''
+    include_routers(dp)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
