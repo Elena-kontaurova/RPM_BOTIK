88858f1a7038d5a76ff390de23b1b10d515747c8
123 <123>
2024-05-15 16:39:48 +0300

    Окончание первой userstory

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 import random
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.filters.command import Command
 from datetime import datetime, timedelta
-from parset import list_of_del
+from aiogram import Router
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from parset import list_of_jokes
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -13,30 +14,51 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token="7069139093:AAHGMroYwnQgEBXwxRRFuUJ5hgSmRVOHoDQ")
 # Диспетчер
 dp = Dispatcher()
+router = Router()
 
 async def send_joke(user_id, sheduled_time):
+    ''' Функция для проверки текущего времени'''
     while True:
         now = datetime.now()
         if now >= sheduled_time:
-            list_of_dels = list_of_del[random.randint(0, len(list_of_del) - 1)]
-            await bot.send_message(user_id, list_of_dels)
+            if list_of_jokes:
+                random_joke = random.choice(list_of_jokes)
+                list_of_jokes.remove(random_joke)
+                await bot.send_message(user_id, random_joke)
+            else:
+                await bot.send_message(user_id, "К сожалению, у меня закончились шутки.")
             break
         await asyncio.sleep(10)
 
 
-@dp.message_handler(command = ['start'])
-async def start(message: types.Message):
+@router.message(Command("start"))
+async def start_ha(message: types.Message):
     '''Хэндлер на команду /start '''
     await message.reply("Приветик? Введите время, в которое хотите получить мем (в формате ЧЧ:MM) ")
 
 
-@dp.message_handler()
-async def 
+@router.message()
+async def get_time(message: types.Message):
+    '''' Метод отправки сообщения в указанное время'''
+    try:
+        time_str = message.text
+        time_obj = datetime.strptime(time_str, '%H:%M')
+        scheduled_time = datetime.now().replace(hour=time_obj.hour,
+                                                minute=time_obj.minute, second=0)
+        if scheduled_time < datetime.now():
+            scheduled_time += timedelta(days=1)
+        asyncio.create_task(send_joke(message.chat.id, scheduled_time))
+        await message.reply(f'Шутка будет отправлена в {scheduled_time.strftime('%H:%M')}')
+    except ValueError:
+        await message.reply("Некорректный формат времени. \
+                            Пожалуйста, введите время в формате ЧЧ:ММ")
 
 
 async def main():
     ''' Запуск процесса поллинга новых апдейтов '''
     await dp.start_polling(bot)
+
+dp.include_routers(router)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- parset.py
+++ parset.py
@@ -1,5 +1,4 @@
 ''' Файл для парсинга мемов'''
-import random
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,15 +9,12 @@ URL = 'https://anekdoty.ru/'
 def parser(url):
     ''' метод получения адреса'''
     response = requests.get(url, timeout=5)
-
-
     bs = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
-
-
     table = bs.find_all('div', attrs={'class': 'holder-body'})
+    jokes = []
     for i in table:
         dela = i.find_all('p')
-        return [c.text for c in dela]
+        jokes.extend([c.text for c in dela])
+    return jokes
 
-list_of_del = parser(URL)
-random.shuffle(list_of_del)
+list_of_jokes = parser(URL)
