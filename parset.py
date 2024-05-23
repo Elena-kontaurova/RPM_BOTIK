f501510b2980496827eb11bef21e8c5eccbe3a23
123 <123>
2024-05-23 11:53:43 +0300

    ghfxd

--- parset.py
+++ parset.py
@@ -1,21 +1,33 @@
-''' Файл для парсинга мемов'''
 import requests
 from bs4 import BeautifulSoup
+import time
 
+URL = f'https://pikabu.ru/community/mem/search'
 
-for i in range(11):
-    URL = 'https://2fan.ru/meme-trendsmems?page={i}'
+def parser(url):
+    ''' метод получения адреса'''
+    response = requests.get(url, timeout=60)
+    response.encoding = 'windows-1251'
 
+    with open('html.log', 'w', encoding='utf-8') as f:
+        f.write(response.text)
 
-    def parser(url):
-        ''' метод получения адреса'''
-        response = requests.get(url, timeout=60)
-        bs = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
-        table = bs.find_all('div', attrs={'class': 'photo'})
-        jokes = []
-        for k in table:
-            dela = k.find('img').get('src')
-            jokes.append(dela)
-        return jokes
-    list_of_jokes = parser(URL)
-    # print(list_of_jokes)
+    bs = BeautifulSoup(response.text, 'html.parser')
+    content = bs.find('div', class_='stories-feed__container')
+    # tables = table.find_all('a')
+    jokes = []
+    for post in content.find_all('article'):
+        story = post.find('div', class_='story-image__content')
+        if story is None:
+            continue
+        img = story.find('img')
+        if img is None:
+            continue
+        src = img.get('src')
+        if src is None or src == '':
+            continue
+        jokes.append(src)
+    return jokes
+
+list_of_jokes = parser(URL)
+print(list_of_jokes)
