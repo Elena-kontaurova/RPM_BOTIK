a542645a21d98bfd8cb6c38f15a6b3ac1c688192
Elena-kontaurova <kontaurova0306@gmail.com>
2024-05-28 18:56:17 +0300

    щшнщш

--- bot/models.py
+++ bot/models.py
@@ -1,7 +1,7 @@
 """Модели для БД"""
 from peewee import SqliteDatabase, Model, ForeignKeyField, \
                         TimeField, IntegerField,\
-                          CharField
+                        CharField, BlobField
 db = SqliteDatabase('sqlite.db')
 
 class Table(Model):
@@ -10,52 +10,48 @@ class Table(Model):
         """Все таблицы будет сохраняться в одной БД"""
         database = db
 
-class User(Table):
-    """Пользователь"""
-    tg_user = IntegerField()
-    time = TimeField(null=True)
+class User(Table): 
+    """Пользователь""" 
+    tg_user = IntegerField() 
+    time = TimeField(null=True) 
 
-class Image(Table):
-    '''  таблица картинок'''
-    url = CharField()
 
-class Tag(Table):
-    ''' таблица тэгов'''
-    name = CharField()
+class Image(Table): 
+    '''  таблица картинок''' 
+    url = CharField() 
+    id = CharField(primary_key=True) 
 
-class ImageTag(Table):
-    ''' таблица картинка и тэг'''
+
+class Tag(Table): 
+    ''' таблица тэгов''' 
+    name = CharField() 
+
+ 
+class ImageTag(Table): 
+    ''' таблица картинка и тэг''' 
     image = ForeignKeyField(
         Image,
         backref='image_tag'
     )
-    tag = ForeignKeyField(
-        Tag,
+    tag = ForeignKeyField( 
+        Tag, 
         backref='image_tag'
     )
 
-class ImageUser(Table):
-    ''' таблица картинка и пользователь'''
-    tg_user = IntegerField()
-    image = ForeignKeyField(Image)
+ 
+class ImageUser(Table): 
+    """ 
+    Модель для хранения изображений пользователей. 
+    """ 
+    id = CharField(primary_key=True) 
+    image = BlobField() 
 
-class ImageManager:
-    # """Класс для управления отправкой картинок пользователям"""
-
-    # @staticmethod
-    # def has_sent_image(user_id, image_id):
-    #     """Проверяет, была ли отправлена данная картинка данному пользователю"""
-    #     try:
-    #         ImageUser.get(ImageUser.tg_user == user_id, ImageUser.image == image_id)
-    #         return True
-
-    # @staticmethod
-    # def send_image(user_id, image_id):
-    #     """Отправляет картинку пользователю и сохраняет информацию об этом"""
-    #     ImageUser.create(tg_user=user_id, image=image_id)
-
-
+class UserImageTag(Table):
+    """Класс для управления отправкой картинок пользователям"""
+    tg_user_id = IntegerField()
+    image = ForeignKeyField(ImageUser, backref='user_image_tags')
+    tag = CharField()
 
 db.connect()
-db.create_tables([User, Image, Tag, ImageTag, ImageUser],  safe=True)
+db.create_tables([User, Image, Tag, ImageTag, ImageUser, UserImageTag],  safe=True)
 db.close()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """Телеграм бот с примеров установки времени рассылки 
 и алгоритмом рассылки сообщений без лишний зхапросов к БД"""
+import random
 import asyncio
 from datetime import time, timedelta, datetime
 from aiogram import Bot, Dispatcher
 from bot.handlers import include_routers
-from bot.models import User
+from bot.models import User, UserImageTag, Image
 from bot.singleton import GlobalVars
 from tok import TOKEN
 from parse import parse_image
@@ -26,9 +27,14 @@ async def sending_messages():
         now_time = datetime.now().time()
         now_time = time(now_time.hour, now_time.minute)
         if GlobalVars.SEND_TIME and GlobalVars.SEND_TIME == now_time:
-            # рассылка уведомлений всем пользователям
             for user in User.filter(time=GlobalVars.SEND_TIME):
-                pass
+                not_sent_images = Image.select().where(~(Image.id << UserImageTag.select(UserImageTag.image).where(UserImageTag.tg_user_id == user.tg_user)))
+                if not_sent_images:
+                    random_image = random.choice(not_sent_images)
+                    await bot.send_photo(user.tg_user, random_image.url)
+            
+            # Сохраняем информацию о том, что изображение было отправлено этому пользователю
+            UserImageTag.create(tg_user_id=user.tg_user, image=random_image.id, tag='')
 
 
             GlobalVars.SEND_TIME = await get_time_notify()
